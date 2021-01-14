6781781ceca50d88afcd74270cca5eb5b8f0c700
HirokiYagou <75972720+HirokiYagou@users.noreply.github.com>
2021-01-14 21:53:52 +0900

    vueによる投稿実装

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,10 +1,13 @@
+import { csrfToken } from "@rails/ujs"
+
 function index() {
   Vue.createApp({
     data: function() {
       return {
         message: '',
         isVisible: false,
-        post: {},
+        posts: [],
+        newPost: {},
       }
     },
     computed: {
@@ -15,25 +18,69 @@ function index() {
         return 'empty'
       }
     },
+    watch: {
+      newPost: {
+        handler: function(next) {
+          this.post = this.posts.unshift(next)
+        },
+      },
+    },
     methods: {
       submit: function() {
-        axios
-          .post('/posts', {
-            content: this.message
+        const sendData = {
+          content: this.message,
+        }
+        fetch('/posts', {
+            method: 'POST',
+            headers: {
+              'X-CSRF-Token': csrfToken(),
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(sendData)
           })
           .then(response => {
-            this.post = response.data
+            return response.json();
+          })
+          .then(data => {
+            this.newPost = data
             this.message = ''
             this.closeForm()
           })
-          .catch(error => console.log(error))
+          .catch(error => {
+            console.log(error)
+          })
       },
       openForm: function() {
         this.isVisible = true
       },
       closeForm: function() {
         this.isVisible = false
+        this.message = '';
       },
+      fetchPosts: function() {
+        fetch('/posts.json')
+          .then(response => {
+            return response.json()
+          })
+          .then(data => {
+            this.posts = data
+          })
+          .catch(error => {
+            console.log(error)
+          });
+      },
+      checked: function(post) {
+        fetch(`posts/${post.id}`)
+          .then(response => {
+            return response.json()
+          })
+          .then(data => {
+            post.checked = data.post.checked
+          })
+      },
+    },
+    mounted: function() {
+      this.fetchPosts()
     },
   }).mount('#index')
 }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,30 +1,9 @@
+const { csrfToken } = require("@rails/ujs")
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import axios from 'axios';
 
 require("../index")
-
-console.log(1234)
-
-axios.get('/posts.json')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.log(error)
-  });
-
-axios.interceptors.request.use((config) => {
-  console.log(5678)
-  if(config.method == 'post' || config.method == 'put' ||
-      config.method == 'patch' || config.method == 'delete') {
-    const csrf_token = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-    config.headers['X-CSRF-Token'] = csrf_token;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
